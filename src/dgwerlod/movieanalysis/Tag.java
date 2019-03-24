c297271d9d31700d809f7506accde04b3aae2d08
DGWerlod <ddeanda333@gmail.com>
2019-03-23 20:03:13 -0700

    Adjusted Tag handling of users and movies
    Signed-off-by: DGWerlod <ddeanda333@gmail.com>

--- src/dgwerlod/movieanalysis/Tag.java
+++ src/dgwerlod/movieanalysis/Tag.java
@@ -1,33 +1,31 @@
 package dgwerlod.movieanalysis;
 
+import java.util.ArrayList;
+
 public class Tag {
 
     private String tag;
-    private int[] userIDs;
-    private int[] movieIDs;
+    private ArrayList<Integer> userIDs; // all users who have applied this tag
+    private ArrayList<Integer> movieIDs;
     private int timestamp;
 
-    public Tag(String tag, int[] userIDs, int[] movieIDs, int timestamp) {
+    public Tag(String tag, ArrayList<Integer> userIDs, ArrayList<Integer> movieIDs, int timestamp) {
         this.tag = tag;
         this.userIDs = userIDs;
         this.movieIDs = movieIDs;
         this.timestamp = timestamp;
     }
 
+    public void addUserID(int id) {
+        userIDs.add(id);
+    }
+
+    public void addMovieID(int id) {
+        movieIDs.add(id);
+    }
+
     public String getTag() {
         return tag;
-    }
-
-    public int[] getUserIDs() {
-        return userIDs;
-    }
-
-    public int[] getMovieIDs() {
-        return movieIDs;
-    }
-
-    public int getTimestamp() {
-        return timestamp;
     }
 
 }
